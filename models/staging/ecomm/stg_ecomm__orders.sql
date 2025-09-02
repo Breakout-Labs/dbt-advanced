e798d7c70cd93f96c51f5303d05861e61d14faec
Frank Gildner <frank.gildner@siemens.com>
2025-09-02 10:09:22 +0000

    Lab 5 completed

--- models/staging/ecomm/stg_ecomm__orders.sql
+++ models/staging/ecomm/stg_ecomm__orders.sql
@@ -3,6 +3,11 @@ with source as (
     from {{ source('ecomm', 'orders') }}
 ),
 
+order_status as (
+    select *
+    from {{ ref('order_status') }}
+),
+
 renamed as (
     select
         *,
@@ -14,26 +19,23 @@ renamed as (
 
 normalize_order_status as (
     select
-        *,
-        -- quick & dirty, will fix later - Mike
-        case 
-            when order_status ilike any(
-                'ordered', 'order_created') then 'Ordered'
-            when lower(order_status) in ('shipped', 'sent')
-                then 'Shipped'
-            when lower(order_status) = 'pending' or lower(order_status) in ('waiting', 'processing', 'payment_pending') then 'Pending'
-            when order_status = 'canceled' or 
-            order_status = 'cancelled' then 'Canceled'
-            when order_status = 'delivered' then 'Delivered'
-            else
-                'Unknown'
-        end as order_status_normalized
+       renamed.*,
+       coalesce(order_status.order_status_normalized, 'Unknown') as order_status
     from renamed
+    left join {{ ref('order_status') }} on (lower(renamed.status) = order_status.order_status)
+),
+
+add_store as (
+    select 
+        nos.*,
+        s.store_name,
+    from normalize_order_status nos
+    left outer join {{ ref('stores') }} s on s.store_id = nos.store_id
 ),
 
 final as (
     select *
-    from normalize_order_status
+    from add_store
 )
 
 select *
