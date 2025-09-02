1e76d3d919674b1bd9ab137fda2ca0b74ac4ff48
Jana Kretschmer <jana.kretschmer@siemens.com>
2025-09-02 10:27:33 +0000

    Lab 5 completed

--- models/staging/ecomm/stg_ecomm__orders.sql
+++ models/staging/ecomm/stg_ecomm__orders.sql
@@ -12,23 +12,19 @@ renamed as (
     from source
 ),
 
+orders_status as (
+    select
+        *
+    from {{ ref('order_status') }}
+),
+
 normalize_order_status as (
     select
-        *,
-        -- quick & dirty, will fix later - Mike
-        case 
-            when order_status ilike any(
-                'ordered', 'order_created') then 'Ordered'
-            when lower(order_status) in ('shipped', 'sent')
-                then 'Shipped'
-            when lower(order_status) = 'pending' or lower(order_status) in ('waiting', 'processing', 'payment_pending') then 'Pending'
-            when order_status = 'canceled' or 
-            order_status = 'cancelled' then 'Canceled'
-            when order_status = 'delivered' then 'Delivered'
-            else
-                'Unknown'
-        end as order_status_normalized
+        renamed.*,
+        coalesce(orders_status.order_status_fixed,'unknown') as orders_status
     from renamed
+        left join orders_status on 
+        renamed.order_status = orders_status.order_status
 ),
 
 final as (
