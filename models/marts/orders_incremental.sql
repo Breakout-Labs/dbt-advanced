39ff489bec24740d1d3a0e57957d914bd079bf4b
Mia Gietmann <mia.gietmann@siemens.com>
2025-09-02 14:09:42 +0000

    Lab 6 done! yay

---
+++ models/marts/orders_incremental.sql
@@ -0,0 +1,61 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key = 'order_id',
+        on_schema_change='append_new_columns'
+    )
+}}
+
+with orders as (
+    select *
+    from {{ ref('stg_ecomm__orders') }}
+),
+
+deliveries as (
+    select *
+    from {{ ref('stg_ecomm__deliveries') }}
+),
+
+deliveries_filtered as (
+    select *
+    from deliveries
+    where delivery_status = 'delivered'
+),
+
+joined as (
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.ordered_at,
+        orders.order_status,
+        orders.total_amount,
+        datediff(
+            'minutes', orders.ordered_at, deliveries_filtered.delivered_at
+        ) as delivery_time_from_order,
+        datediff(
+            'minutes',
+            deliveries_filtered.picked_up_at,
+            deliveries_filtered.delivered_at
+        ) as delivery_time_from_collection
+    from orders
+    left join deliveries_filtered
+        on orders.order_id = deliveries_filtered.order_id
+),
+
+final as (
+    select *,
+    CURRENT_TIMESTAMP as last_updated 
+    from joined
+)
+
+select *
+from final
+
+{% if is_incremental() %}
+
+  -- this filter will only be applied on an incremental run
+  -- (uses >= to include records whose timestamp occurred since the last run of this model)
+  -- (If event_time is NULL or the table is truncated, the condition will always be true and load all records)
+where ordered_at >= (select dateadd('day',-3,max(ordered_at)) from {{ this }})
+
+{% endif %}
