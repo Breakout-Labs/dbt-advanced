4ce2e6e2ed898447d18c22bdde1f90dabc8d6518
Nils Rek <nils.rek@siemens.com>
2025-09-03 10:54:11 +0000

    finished lab 9

--- models/staging/ecomm/stg_ecomm__orders.sql
+++ models/staging/ecomm/stg_ecomm__orders.sql
@@ -1,6 +1,13 @@
-with source as (
-    select *
-    from {{ source('ecomm', 'orders') }}
+with sources as (
+    {{
+        dbt_utils.union_relations(
+            relations=[
+                source('ecomm', 'orders_us'),
+                source('ecomm', 'orders_de'),
+                source('ecomm', 'orders_au')
+            ],
+        )
+    }}
 ),
 
 renamed as (
@@ -9,7 +16,26 @@ renamed as (
         id as order_id,
         created_at as ordered_at,
         status as order_status
-    from source
+    from sources
+),
+
+redefined_store_id as (
+    select * exclude(store_id),
+        case 
+            when _dbt_source_relation ilike '%orders_us' then 1
+            when _dbt_source_relation ilike '%orders_de' then 2
+            when _dbt_source_relation ilike '%orders_au' then 3
+        end as store_id
+    from renamed
+),
+
+deduplicated_cte as (
+  {{ dbt_utils.deduplicate(
+      relation='redefined_store_id',
+      partition_by='customer_id',
+      order_by='customer_id desc',
+     )
+  }}
 ),
 
 order_status_lookup as (
@@ -19,11 +45,11 @@ order_status_lookup as (
 
 normalize_order_status as (
     select
-        renamed.*,
+        deduplicated_cte.*,
         coalesce(order_status_lookup.order_status_normalized, 'Unknown') as order_status_normalized
-    from renamed
+    from deduplicated_cte
     left join order_status_lookup
-    on lower(renamed.order_status) = order_status_lookup.order_status
+    on lower(deduplicated_cte.order_status) = order_status_lookup.order_status
 ),
 
 final as (
