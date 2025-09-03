43d5e892a0c868fb71700bbfd8067f2ef0872048
Max Heckel <max.heckel@siemens.com>
2025-09-03 10:46:15 +0000

    Completed lab 9!

--- models/staging/ecomm/stg_ecomm__orders.sql
+++ models/staging/ecomm/stg_ecomm__orders.sql
@@ -1,7 +1,13 @@
-with source as (
-    select *
-    from {{ source('ecomm', 'orders') }}
-),
+with source as ( {{
+    dbt_utils.union_relations(
+        relations=[
+            source('ecomm', 'orders_us'),
+            source('ecomm', 'orders_de'),
+            source('ecomm', 'orders_au')
+        ],
+    )
+
+}}),
 
 
 order_status_1 as (
@@ -9,12 +15,23 @@ order_status_1 as (
     from {{ ref('order_status') }}
 ),
 
+add_store_id as (
+    select
+        * exclude (store_id),   
+        case
+            when _dbt_source_relation ilike '%orders_us' then 1
+            when _dbt_source_relation ilike '%orders_de' then 2
+            when _dbt_source_relation ilike '%orders_au' then 3
+        end as store_id            
+    from source
+),
+
 renamed as (
     select
         *,
         id as order_id,
         created_at as ordered_at
-    from source
+    from add_store_id
 ),
 
 normalize_order_status as (
@@ -30,7 +47,17 @@ normalize_order_status as (
 final as (
     select *, current_timestamp as last_updated
     from normalize_order_status
+),
+
+deduplicated as (
+    {{
+        dbt_utils.deduplicate(
+            relation='final',
+            partition_by='order_id',
+            order_by='_synced_at desc'
+        )
+    }}
 )
 
 select *
-from final
+from deduplicated
