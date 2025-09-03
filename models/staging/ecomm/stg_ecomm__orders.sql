876542fb2c38227ab921ebf1b99073a1f31df688
Robert Cziko <robert.cziko@siemens.com>
2025-09-03 11:16:37 +0000

    Lab 9 completed!

--- models/staging/ecomm/stg_ecomm__orders.sql
+++ models/staging/ecomm/stg_ecomm__orders.sql
@@ -1,6 +1,13 @@
 with source as (
-    select *
-    from {{ source('ecomm', 'orders') }}
+    {{
+    dbt_utils.union_relations(
+        relations=[
+            source('ecomm', 'orders_us'),
+            source('ecomm', 'orders_de'),
+            source('ecomm', 'orders_au')
+        ],
+    )
+}}
 ),
 
 order_status_csv as (
@@ -8,12 +15,23 @@ order_status_csv as (
     from {{ ref('order_status') }}
 ),
 
+add_store_id as (
+    select
+        * exclude (store_id),    -- Omit original store_id column
+        case
+            when _dbt_source_relation ilike '%orders_us' then 1
+            when _dbt_source_relation ilike '%orders_de' then 2
+            when _dbt_source_relation ilike '%orders_au' then 3
+        end as store_id            -- Add calculated store_id
+    from source
+),
+
 renamed as (
     select
         *,
         id as order_id,
         created_at as ordered_at
-    from source
+    from add_store_id
 ),
 
 normalize_order_status as (
@@ -27,7 +45,15 @@ normalize_order_status as (
 final as (
     select *
     from normalize_order_status
+),
+
+deduplicated_cte as (
+  {{ dbt_utils.deduplicate(
+      relation='final',
+      partition_by='order_id',
+      order_by='_synced_at desc',
+     )
+  }}
 )
 
-select *
-from final
+select * from deduplicated_cte
