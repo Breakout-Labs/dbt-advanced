8306b7d0bd80d6d185c0f086bc236de55d6c2199
Mia Gietmann <mia.gietmann@siemens.com>
2025-09-03 09:42:43 +0000

    a

--- models/marts/orders_incremental.sql
+++ models/marts/orders_incremental.sql
@@ -56,6 +56,6 @@ from final
   -- this filter will only be applied on an incremental run
   -- (uses >= to include records whose timestamp occurred since the last run of this model)
   -- (If event_time is NULL or the table is truncated, the condition will always be true and load all records)
-where ordered_at >= (select dateadd('day',-3,max(ordered_at)) from {{ this }})
+where ordered_at > (select dateadd('day',-3,max(ordered_at)) from {{ this }})
 
 {% endif %}
