7c49bd1a3696fe418a2c862fb0ccbbfdcb645929
Jonah Marie Lawas <jonah-marie.lawas@siemens.com>
2025-09-03 16:53:44 +0000

    Lab 8 completed!

--- models/staging/ecomm/stg_ecomm__orders.sql
+++ models/staging/ecomm/stg_ecomm__orders.sql
@@ -1,12 +1,29 @@
-with source as (
-    select *
-    from {{ source('ecomm', 'orders') }}
+with sources as (
+    {{
+        dbt_utils.union_relations(
+            relations=[
+                source('ecomm', 'orders_us'),
+                source('ecomm', 'orders_de'),
+                source('ecomm', 'orders_au')
+            ],
+        )
+    }}
 ),
-
 
 order_status as (
     select *
     from {{ ref('order_status') }}
+),
+
+add_store_id as (
+    select
+        * exclude (store_id),    -- Does not include column for store id
+        case
+            when _dbt_source_relation ilike '%orders_us' then 1
+            when _dbt_source_relation ilike '%orders_de' then 2
+            when _dbt_source_relation ilike '%orders_au' then 3
+        end as store_id            -- Final store id 
+    from sources
 ),
 
 renamed as (
@@ -14,23 +31,44 @@ renamed as (
         *,
         id as order_id,
         created_at as ordered_at
-    from source
+    from add_store_id
 ),
 
 normalize_order_status as (
     select
         renamed.*,
-        coalesce(order_status.order_status_normalized, 'Unknown') as order_status --don't know coalesce!
+        coalesce(order_status.order_status_normalized, 'Unknown') as order_status
     from renamed
     left join order_status on (
         lower(renamed.status) = order_status.order_status
     )
 ),
 
-final as (
-    select *
-    from normalize_order_status
+deduplicated as (
+    {{
+        dbt_utils.deduplicate(
+            relation='normalize_order_status',
+            partition_by='order_id',
+            order_by='_synced_at desc'
+        )
+    }}
 )
 
-select *
-from final
+select
+    *
+from deduplicated
+
+--final as (
+    --select *
+    --from normalize_order_status
+--)
+
+--select *
+--from final
+
+
+
+
+
+
+
