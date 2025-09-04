fe4ff4e84f2f790a0a9ed6f001026d86ce45d1e0
Max Heckel <max.heckel@siemens.com>
2025-09-04 10:38:29 +0000

    Lab 10 completed!

--- models/marts/ecomm/mrt_ecomm_orders.sql
+++ models/marts/ecomm/mrt_ecomm_orders.sql
@@ -1,5 +1,6 @@
 {{
     config(
+        materialized='table',
         snowflake_warehouse='TRANSFORMING_S'
     )
 }}
@@ -40,15 +41,20 @@ joined as (
             'minutes',
             deliveries_filtered.picked_up_at,
             deliveries_filtered.delivered_at
-        ) as delivery_time_from_collection
+        ) as delivery_time_from_collection,
+        greatest_ignore_nulls(orders._synced_at, deliveries_filtered._synced_at) as source_last_updated
     from orders
     left join deliveries_filtered
         on orders.order_id = deliveries_filtered.order_id
     left join stores using (store_id)
 ),
-
+-- adding pk, hk and last_updated to the model 
 final as (
-    select *
+    select 
+    {{ dbt_utils.generate_surrogate_key(['order_id']) }} as pk_orders,
+    {{ dbt_utils.generate_surrogate_key(['customer_id']) }} as hk_customer,
+    current_timestamp() as last_updated,
+    *
     from joined
 )
 
