7bab8a760db79a7b0244e6fd6f68df4567a36743
Max Heckel <max.heckel@siemens.com>
2025-09-04 14:25:21 +0000

    done :)

--- analyses/zipcodes.sql
+++ analyses/zipcodes.sql
@@ -1,8 +1,8 @@
 select
     country,
-    state.value['state'] as states,
-    zip_code.value['city'] as city,
-    zip_code.value['zipcode'] as zipcode_per_city
+    state.value['state']::varchar as states,
+    zip_code.value['city']::varchar as city,
+    zip_code.value['zipcode']::varchar as zipcode_per_city
 from raw.geo.countries,
     lateral flatten(input => states) as state,
     lateral flatten(input => state.value['zipcodes']) as zip_code
