d34bddf5148bd5c4cacf794ec89639c2302c8e34
Mia Gietmann <mia.gietmann@siemens.com>
2025-09-04 10:57:54 +0000

    Lab 10 completed!

--- models/marts/ecomm/mrt_ecomm_orders.sql
+++ models/marts/ecomm/mrt_ecomm_orders.sql
@@ -28,15 +28,26 @@ joined as (
             'minutes',
             deliveries_filtered.picked_up_at,
             deliveries_filtered.delivered_at
-        ) as delivery_time_from_collection
+        ) as delivery_time_from_collection,
+        greatest_ignore_nulls(orders._synced_at, deliveries_filtered._synced_at) as source_last_updated
     from orders
     left join deliveries_filtered
         on orders.order_id = deliveries_filtered.order_id
 ),
 
+data_prod_conventions as (
+    select 
+    *,
+    {{ dbt_utils.generate_surrogate_key(['order_id']) }} as pk_orders,
+    {{ dbt_utils.generate_surrogate_key(['customer_id']) }} as hk_customer,
+    current_timestamp() as last_updated 
+    from joined
+
+),
+
 final as (
     select *
-    from joined
+    from data_prod_conventions
 )
 
 select *
