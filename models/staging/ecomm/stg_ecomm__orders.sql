724bf1c2adb6585ad208663d553ab9c94210b0e1
Mia Gietmann <mia.gietmann@siemens.com>
2025-09-04 10:03:08 +0000

    corrections and improvements to the orders

--- models/staging/ecomm/stg_ecomm__orders.sql
+++ models/staging/ecomm/stg_ecomm__orders.sql
@@ -1,7 +1,16 @@
-with source as (
-    select *
-    from {{ source('ecomm', 'orders') }}
+with sources as (
+    {{
+    dbt_utils.union_relations(
+        relations=[
+            source('ecomm', 'orders_us'),
+            source('ecomm', 'orders_de'),
+            source('ecomm', 'orders_au')
+        ],
+    )
+}}
 ),
+
+
 
 stores as (
     select *
@@ -13,22 +22,45 @@ order_status as (
     from {{ ref('order_status') }}
 ),
 
+add_store_id as (
+    select
+        * exclude (store_id),    -- Omit original store_id column
+        case
+            when _dbt_source_relation ilike '%orders_us' then 1
+            when _dbt_source_relation ilike '%orders_de' then 2
+            when _dbt_source_relation ilike '%orders_au' then 3
+        end as store_id            -- Add calculated store_id
+    from sources
+),
+
 
 renamed as (
     select
         *,
         id as order_id,
         created_at as ordered_at,
-        status as order_status
-    from source
+        status as order_status,
+
+    from add_store_id
 ),
+
+deduplicated as (
+    {{
+        dbt_utils.deduplicate(
+            relation='renamed',
+            partition_by='order_id',
+            order_by='_synced_at desc'
+        )
+    }}
+),
+
 
 store_mapping as (
     select 
-    renamed.*,
+    deduplicated.*,
     stores.store_name
-    from renamed
-    left join stores on stores.store_id = renamed.store_id
+    from deduplicated
+    left join stores on stores.store_id = deduplicated.store_id
 ),
 
 
