4802d0c8f687db86a8056550d794192445aaaac4
Shivani Inti <naga-surya-shivani.inti@siemens.com>
2025-11-10 18:41:23 +0000

    Lab 5 Completed

--- models/marts/orders.sql
+++ models/marts/orders.sql
@@ -1,5 +1,6 @@
 {{ config(materialized='table') }}
 
+-- models/orders.sql
 with orders as (
     select *
     from {{ ref('stg_ecomm__orders') }}
@@ -16,6 +17,11 @@ deliveries_filtered as (
     where delivery_status = 'delivered'
 ),
 
+store_names as (
+    select *
+    from {{ ref('stores') }}
+),
+
 joined as (
     select
         orders.order_id,
@@ -23,6 +29,7 @@ joined as (
         orders.ordered_at,
         orders.order_status,
         orders.total_amount,
+        store_names.store_name,
         datediff(
             'minutes', orders.ordered_at, deliveries_filtered.delivered_at
         ) as delivery_time_from_order,
@@ -32,8 +39,10 @@ joined as (
             deliveries_filtered.delivered_at
         ) as delivery_time_from_collection
     from orders
-    left join deliveries_filtered
-        on orders.order_id = deliveries_filtered.order_id
+    left join
+        deliveries_filtered
+        on (orders.order_id = deliveries_filtered.order_id)
+    left join store_names on (orders.store_id = store_names.store_id)
 ),
 
 final as (
