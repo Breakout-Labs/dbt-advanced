62186e15e569ab5f79990e31a6f683eeee2237f1
Kanchan Joshi <kanchan.joshi@siemens.com>
2025-11-10 17:24:36 +0000

    Lab 5 completed!

--- models/staging/ecomm/stg_ecomm__orders.sql
+++ models/staging/ecomm/stg_ecomm__orders.sql
@@ -12,11 +12,17 @@ renamed as (
     from source
 ),
 
+order_status_seed as (
+    select *
+    from {{ ref('order_status') }}
+),
+
 normalize_order_status as (
     select
-        *,
+        renamed.* exclude(order_status),
         -- quick & dirty, will fix later - Mike
-        case 
+        order_status_seed.order_status_normalized
+{#         case 
             when order_status ilike any(
                 'ordered', 'order_created') then 'Ordered'
             when lower(order_status) in ('shipped', 'sent')
@@ -27,8 +33,10 @@ normalize_order_status as (
             when order_status = 'delivered' then 'Delivered'
             else
                 'Unknown'
-        end as order_status_normalized
+        end as order_status_normalized #}
     from renamed
+    left join order_status_seed
+    on renamed.order_status = order_status_seed.order_status
 ),
 
 final as (
