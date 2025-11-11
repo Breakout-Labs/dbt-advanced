660cc9166cf5b6e323675b81b1095618b9e44793
frank.hettmer@siemens.com <frank.hettmer@siemens.com>
2025-11-11 14:24:07 +0000

    Lab 5

--- models/marts/orders.sql
+++ models/marts/orders.sql
@@ -1,3 +1,4 @@
+-- models/orders.sql
 with orders as (
     select *
     from {{ ref('stg_ecomm__orders') }}
@@ -14,6 +15,11 @@ deliveries_filtered as (
     where delivery_status = 'delivered'
 ),
 
+store_names as (
+    select *
+    from {{ ref('stores') }}
+),
+
 joined as (
     select
         orders.order_id,
@@ -21,6 +27,7 @@ joined as (
         orders.ordered_at,
         orders.order_status,
         orders.total_amount,
+        store_names.store_name,
         datediff(
             'minutes', orders.ordered_at, deliveries_filtered.delivered_at
         ) as delivery_time_from_order,
@@ -30,8 +37,10 @@ joined as (
             deliveries_filtered.delivered_at
         ) as delivery_time_from_collection
     from orders
-    left join deliveries_filtered
-        on orders.order_id = deliveries_filtered.order_id
+    left join
+        deliveries_filtered
+        on (orders.order_id = deliveries_filtered.order_id)
+    left join store_names on (orders.store_id = store_names.store_id)
 ),
 
 final as (
---
+++ models/marts/orders_seed.sql
@@ -0,0 +1,52 @@
+-- models/orders.sql
+with orders as (
+    select *
+    from {{ ref('stg_ecomm__orders') }}
+),
+
+deliveries as (
+    select *
+    from {{ ref('stg_ecomm__deliveries') }}
+),
+
+deliveries_filtered as (
+    select *
+    from deliveries
+    where delivery_status = 'delivered'
+),
+
+store_names as (
+    select *
+    from {{ ref('stores') }}
+),
+
+joined as (
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.ordered_at,
+        orders.order_status,
+        orders.total_amount,
+        store_names.store_name,
+        datediff(
+            'minutes', orders.ordered_at, deliveries_filtered.delivered_at
+        ) as delivery_time_from_order,
+        datediff(
+            'minutes',
+            deliveries_filtered.picked_up_at,
+            deliveries_filtered.delivered_at
+        ) as delivery_time_from_collection
+    from orders
+    left join
+        deliveries_filtered
+        on (orders.order_id = deliveries_filtered.order_id)
+    left join store_names on (orders.store_id = store_names.store_id)
+),
+
+final as (
+    select *
+    from joined
+)
+
+select *
+from final
