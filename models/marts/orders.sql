ff2d332b4628374e48b684f509c8690210caa777
Gideon Fernandez <gideon@velocitysense.com>
2025-11-12 18:17:39 +0000

    Lab 10 done

--- models/marts/orders.sql
+++ models/marts/orders.sql
@@ -29,7 +29,9 @@ store_names as (
 joined as (
 
     select
+        {{ dbt_utils.generate_surrogate_key(['orders.order_id']) }} as pk_orders,
         orders.order_id,
+        {{ dbt_utils.generate_surrogate_key(['orders.customer_id']) }} as hk_customer,
         orders.customer_id,
         orders.ordered_at,
         orders.order_status,
@@ -50,7 +52,9 @@ joined as (
             'minutes',
             deliveries_filtered.picked_up_at,
             deliveries_filtered.delivered_at
-        ) as delivery_time_from_collection
+        ) as delivery_time_from_collection,
+        greatest_ignore_nulls(orders._synced_at, deliveries_filtered._synced_at) as source_last_updated,
+        current_timestamp() as last_updated
     from orders
     left join deliveries_filtered
         on orders.order_id = deliveries_filtered.order_id
