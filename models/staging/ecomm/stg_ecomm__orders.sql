e2d5d60754df6d64fdb0702432d748dc881bedd2
Deanna Strausbaugh <deanna.strausbaugh@siemens.com>
2025-11-12 18:52:35 +0000

    lab 8 and 9 updates

--- models/staging/ecomm/stg_ecomm__orders.sql
+++ models/staging/ecomm/stg_ecomm__orders.sql
@@ -1,13 +1,31 @@
 -- models/staging/stg_ecomm__orders.sql
-with source as (
+with sources as (
     select *
-    from {{ source('ecomm', 'orders') }}
+    from {{
+    dbt_utils.union_relations(
+        relations=[
+            source('ecomm', 'orders_us'),
+            source('ecomm', 'orders_de'),
+            ref('_orders_au_deduped')
+        ],
+    )
+    }}  
 ),
-
 
 order_status as (
     select *
     from {{ ref('order_status') }}
+),
+
+add_store_id as (
+    select
+        * exclude (store_id),
+        case
+            when _dbt_source_relation ilike '%orders_us' then 1
+            when _dbt_source_relation ilike '%orders_de' then 2
+            when _dbt_source_relation ilike '%_orders_au_deduped' then 3
+        end as store_id
+    from sources
 ),
 
 renamed as (
@@ -15,7 +33,7 @@ renamed as (
         *,
         id as order_id,
         created_at as ordered_at
-    from source
+    from add_store_id
 ),
 
 normalize_order_status as (
