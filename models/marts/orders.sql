e914b9b23ceeb9e675311228f6464fd27e1ee7bc
Samuele Crescenti <samuele@breakout-labs.com>
2026-02-25 07:28:00 +0000

    lab 5

--- models/marts/orders.sql
+++ models/marts/orders.sql
@@ -16,6 +16,11 @@ deliveries_filtered as (
     where delivery_status = 'delivered'
 ),
 
+store_names as (
+    select *
+    from {{ ref('stores') }}
+),
+
 joined as (
     select
         orders.order_id,
@@ -23,6 +28,7 @@ joined as (
         orders.ordered_at,
         orders.order_status,
         orders.total_amount,
+        store_names.store_name,
         datediff(
             'minutes', orders.ordered_at, deliveries_filtered.delivered_at
         ) as delivery_time_from_order,
@@ -34,6 +40,8 @@ joined as (
     from orders
     left join deliveries_filtered
         on orders.order_id = deliveries_filtered.order_id
+    left join store_names 
+        on (orders.store_id = store_names.store_id)
 ),
 
 final as (
