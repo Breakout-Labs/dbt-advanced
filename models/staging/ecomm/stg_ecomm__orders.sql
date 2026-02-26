5bfe59e0f31faeaf13725ca1d35b7e2f36a1697a
Samuele Crescenti <samuele@breakout-labs.com>
2026-02-26 07:46:32 +0000

    Lab 9 completed!

--- models/staging/ecomm/stg_ecomm__orders.sql
+++ models/staging/ecomm/stg_ecomm__orders.sql
@@ -1,6 +1,13 @@
-with source as (
-    select *
-    from {{ source('ecomm', 'orders') }}
+with sources as (
+    {{
+        dbt_utils.union_relations(
+            relations=[
+                source('ecomm', 'orders_us'),
+                source('ecomm', 'orders_de'),
+                source('ecomm', 'orders_au')
+            ],
+        )
+    }}
 ),
 
 order_status as (
@@ -8,12 +15,23 @@ order_status as (
     from {{ ref('order_status') }}
 ),
 
+add_store_id as (
+    select
+        * exclude (store_id),    -- Omit original store_id column
+        case
+            when _dbt_source_relation ilike '%orders_us' then 1
+            when _dbt_source_relation ilike '%orders_de' then 2
+            when _dbt_source_relation ilike '%_orders_au' then 3
+        end as store_id            -- Add calculated store_id
+    from sources
+),
+
 renamed as (
     select
         *,
         id as order_id,
         created_at as ordered_at
-    from source
+    from add_store_id
 ),
 
 normalize_order_status as (
@@ -21,8 +39,9 @@ normalize_order_status as (
         renamed.*,
         coalesce(order_status.order_status_normalized, 'Unknown') as order_status
     from renamed
-    left join order_status 
-        on lower(renamed.status) = order_status.order_status
+    left join order_status on (
+        lower(renamed.status) = order_status.order_status
+    )
 ),
 
 final as (
