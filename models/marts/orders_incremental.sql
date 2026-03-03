e92d01f9395ba1f1702c8617688459677cf9b724
Christian Wenzel <christian.wenzel@siemens.com>
2026-03-03 15:14:35 +0000

    Lab 6 completed!

---
+++ models/marts/orders_incremental.sql
@@ -0,0 +1,57 @@
+{{ config(materialized='incremental',
+unique_key = 'order_id') }}
+
+with orders as (
+    select *
+    from {{ ref('stg_ecomm__orders') }}
+    {% if is_incremental() %}
+        -- this filter will only be applied on an incremental run
+        where created_at > (select dateadd('day', -3, max(ordered_at)) from {{ this }}) 
+    {% endif %}
+    
+),
+
+deliveries as (
+    select *
+    , current_timestamp() AS last_updated
+    from {{ ref('stg_ecomm__deliveries') }}
+),
+
+deliveries_filtered as (
+    select *
+    from deliveries
+    where delivery_status = 'delivered'
+),
+
+joined as (
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.ordered_at,
+        orders.order_status,
+        orders.total_amount,
+        datediff(
+            'minutes', orders.ordered_at, deliveries_filtered.delivered_at
+        ) as delivery_time_from_order,
+        datediff(
+            'minutes',
+            deliveries_filtered.picked_up_at,
+            deliveries_filtered.delivered_at
+        ) as delivery_time_from_collection
+    , orders.store_id
+    from orders
+    left join deliveries_filtered
+        on orders.order_id = deliveries_filtered.order_id
+),
+
+addingStores as (
+    select j.*, s.store_name from joined j left join {{ ref('stores') }} s ON j.store_id = s.store_id
+),
+
+final as (
+    select *
+    from addingStores 
+)
+
+select *
+from final
