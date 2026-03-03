e66516ba8a5ac0556eb1d007bebbf1d1c142d56c
Kornél Naszály <kornel.naszaly@evosoft.com>
2026-03-03 11:35:37 +0000

    Lab 5 completed!

--- models/staging/ecomm/stg_ecomm__orders.sql
+++ models/staging/ecomm/stg_ecomm__orders.sql
@@ -1,4 +1,4 @@
-with source as (
+with source as ( 
     select *
     from {{ source('ecomm', 'orders') }}
 ),
@@ -12,23 +12,20 @@ renamed as (
     from source
 ),
 
+order_status_mapping as (
+    select
+        order_status,
+        order_status_normalized
+    from {{ ref('order_status') }}
+),
+
 normalize_order_status as (
     select
-        *,
-        -- quick & dirty, will fix later - Mike
-        case 
-            when order_status ilike any(
-                'ordered', 'order_created') then 'Ordered'
-            when lower(order_status) in ('shipped', 'sent')
-                then 'Shipped'
-            when lower(order_status) = 'pending' or lower(order_status) in ('waiting', 'processing', 'payment_pending') then 'Pending'
-            when order_status = 'canceled' or 
-            order_status = 'cancelled' then 'Canceled'
-            when order_status = 'delivered' then 'Delivered'
-            else
-                'Unknown'
-        end as order_status_normalized
+        renamed.*,
+        coalesce(order_status_mapping.order_status_normalized, 'Unknown') as order_status_normalized
     from renamed
+    left join order_status_mapping
+    on lower(renamed.order_status) = lower(order_status_mapping.order_status)
 ),
 
 final as (
