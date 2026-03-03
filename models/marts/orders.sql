30d21e7ac3d0d42ca6c5616d84dc512c4e3a5075
Lea Huettenhofer <lea.huettenhofer@siemens.com>
2026-03-03 14:24:28 +0000

    Lab 5 tasks

--- models/marts/orders.sql
+++ models/marts/orders.sql
@@ -15,6 +15,9 @@ deliveries_filtered as (
     from deliveries
     where delivery_status = 'delivered'
 ),
+stores as (
+select * from {{ ref('stores') }}
+),
 
 joined as (
     select
@@ -23,6 +26,7 @@ joined as (
         orders.ordered_at,
         orders.order_status,
         orders.total_amount,
+        stores.store_name,
         datediff(
             'minutes', orders.ordered_at, deliveries_filtered.delivered_at
         ) as delivery_time_from_order,
@@ -34,6 +38,7 @@ joined as (
     from orders
     left join deliveries_filtered
         on orders.order_id = deliveries_filtered.order_id
+    left join stores using (store_id)
 ),
 
 final as (
--- models/staging/ecomm/stg_ecomm__orders.sql
+++ models/staging/ecomm/stg_ecomm__orders.sql
@@ -8,33 +8,39 @@ renamed as (
         *,
         id as order_id,
         created_at as ordered_at,
-        status as order_status
+        lower(status) as order_status
     from source
 ),
 
 normalize_order_status as (
     select
-        *,
+renamed.*,
         -- quick & dirty, will fix later - Mike
-        case 
-            when order_status ilike any(
-                'ordered', 'order_created') then 'Ordered'
-            when lower(order_status) in ('shipped', 'sent')
-                then 'Shipped'
-            when lower(order_status) = 'pending' or lower(order_status) in ('waiting', 'processing', 'payment_pending') then 'Pending'
-            when order_status = 'canceled' or 
-            order_status = 'cancelled' then 'Canceled'
-            when order_status = 'delivered' then 'Delivered'
-            else
-                'Unknown'
-        end as order_status_normalized
+--             case 
+--                 when order_status ilike any(
+--                     'ordered', 'order_created') then 'Ordered'
+--                 when lower(order_status) in ('shipped', 'sent')
+--                     then 'Shipped'
+--                 when lower(order_status) = 'pending' or lower(order_status) in ('waiting', 'processing', 'payment_pending') then 'Pending'
+--                 when order_status = 'canceled' or 
+--                 order_status = 'cancelled' then 'Canceled'
+--                 when order_status = 'delivered' then 'Delivered'
+--                 else
+--                     'Unknown'
+--             end as order_status_normalized
+
+
+ coalesce(order_status_seed.order_status_normalized, 'Unknown') as order_status_normalized
     from renamed
+    left join {{ ref('order_status') }} order_status_seed
+        on order_status_seed.order_status = renamed.order_status
 ),
 
 final as (
     select *
     from normalize_order_status
 )
+
 
 select *
 from final
