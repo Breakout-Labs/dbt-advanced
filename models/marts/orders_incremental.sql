906c66c3ca485aa817779dc7042227c2b97483c7
Luis Matosinhos <luis.fernandes-matosinhos@siemens.com>
2026-03-03 15:19:46 +0000

    Lab 6 completed!

--- models/marts/orders_incremental.sql
+++ models/marts/orders_incremental.sql
@@ -1,8 +1,15 @@
-{{ config(materialized='table') }}
+{{ config(materialized='incremental',
+        unique_key='order_id',
+        on_schema_change='append_new_columns') }}
 
 with orders as (
     select *
     from {{ ref('stg_ecomm__orders') }}
+
+    {% if is_incremental() %}
+        -- this filter will only be applied on an incremental run
+        where ordered_at >= (select dateadd('day',-3,max(ordered_at)) from {{ this }}) 
+    {% endif %}
 ),
 
 deliveries as (
@@ -16,11 +23,6 @@ deliveries_filtered as (
     where delivery_status = 'delivered'
 ),
 
-store_names as (
-    select *
-    from {{ ref('stores') }}
-),
-
 joined as (
     select
         orders.order_id,
@@ -28,7 +30,6 @@ joined as (
         orders.ordered_at,
         orders.order_status,
         orders.total_amount,
-        store_names.store_name,
         datediff(
             'minutes', orders.ordered_at, deliveries_filtered.delivered_at
         ) as delivery_time_from_order,
@@ -40,11 +41,11 @@ joined as (
     from orders
     left join deliveries_filtered
         on orders.order_id = deliveries_filtered.order_id
-    left join store_names on orders.store_id = store_names.store_id
 ),
 
 final as (
-    select *
+    select *,
+    current_timestamp() as last_updated
     from joined
 )
 
