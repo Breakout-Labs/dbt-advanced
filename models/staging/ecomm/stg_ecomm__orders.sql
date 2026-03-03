e1fe95171f77e754f9238f63ec77e36aca00e128
Luis Henriques <luis.vicente-henriques@siemens.com>
2026-03-03 11:42:20 +0000

    Lab 5 completed!

--- models/staging/ecomm/stg_ecomm__orders.sql
+++ models/staging/ecomm/stg_ecomm__orders.sql
@@ -8,27 +8,36 @@ renamed as (
         *,
         id as order_id,
         created_at as ordered_at,
-        status as order_status
+        lower(status) as order_status
     from source
+),
+
+order_status as (
+    select *
+    from {{ ref('order_status') }}
 ),
 
 normalize_order_status as (
     select
-        *,
+        renamed.* exclude(order_status),
         -- quick & dirty, will fix later - Mike
-        case 
-            when order_status ilike any(
-                'ordered', 'order_created') then 'Ordered'
-            when lower(order_status) in ('shipped', 'sent')
-                then 'Shipped'
-            when lower(order_status) = 'pending' or lower(order_status) in ('waiting', 'processing', 'payment_pending') then 'Pending'
-            when order_status = 'canceled' or 
-            order_status = 'cancelled' then 'Canceled'
-            when order_status = 'delivered' then 'Delivered'
-            else
-                'Unknown'
-        end as order_status_normalized
+        -- case 
+        --     when order_status ilike any(
+        --         'ordered', 'order_created') then 'Ordered'
+        --     when lower(order_status) in ('shipped', 'sent')
+        --         then 'Shipped'
+        --     when lower(order_status) = 'pending' or lower(order_status) in ('waiting', 'processing', 'payment_pending') then 'Pending'
+        --     when order_status = 'canceled' or 
+        --     order_status = 'cancelled' then 'Canceled'
+        --     when order_status = 'delivered' then 'Delivered'
+        --     else
+        --         'Unknown'
+        -- end as order_status_normalized
+        coalesce(order_status.order_status_normalized, 'Unknown') as order_status
     from renamed
+    left join order_status on (
+        renamed.order_status = order_status.order_status
+    )
 ),
 
 final as (
