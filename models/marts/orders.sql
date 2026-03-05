eb05a22bc095563f6cfdf60e7e0124bcbc006e27
Inês Cadete <ines.cadete@siemens.com>
2026-03-05 11:33:13 +0000

    lab 10- done

--- models/marts/orders.sql
+++ models/marts/orders.sql
@@ -28,8 +28,10 @@ store_names as (
 
 joined as (
     select
-        orders.order_id,
+        orders.order_id, -- it's better practice to keep the original values- unless it's sensitive data, and this way it keeps the meaning
+        {{ dbt_utils.generate_surrogate_key(['orders.order_id']) }} as pk_orders,
         orders.customer_id,
+        {{ dbt_utils.generate_surrogate_key(['orders.customer_id']) }} as hk_customer,
         orders.ordered_at,
         orders.order_status,
         orders.total_amount,
@@ -41,7 +43,9 @@ joined as (
             'minutes',
             deliveries_filtered.picked_up_at,
             deliveries_filtered.delivered_at
-        ) as delivery_time_from_collection
+        ) as delivery_time_from_collection,
+        greatest_ignore_nulls(orders._synced_at, deliveries_filtered._synced_at) as source_last_updated,
+        current_timestamp() as last_updated
     from orders
     left join deliveries_filtered
         on orders.order_id = deliveries_filtered.order_id
